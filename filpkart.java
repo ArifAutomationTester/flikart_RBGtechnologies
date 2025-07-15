bac9dd46eb861cb0d18ed583584825876ac49264
ArifAutomationTester <masuldararif09@gmail.com>
2025-07-15 12:33:54 +0530

    modify

--- filpkart.java
+++ filpkart.java
@@ -4,6 +4,10 @@ public class flipkart_loging
 public static void main(String[] arif)
 {
 System.out,println("hello");
+System.out,println("buddey");
+System.out,println("who");
+System.out,println("are");
+System.out,println("You");
 
 }
 }
